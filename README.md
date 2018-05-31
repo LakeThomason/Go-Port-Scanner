85ada8bcd2a43bd17e0bf8049e16fa2b5b45d218
Lake <lakesainthomason@gmail.com>
2018-05-30 21:32:28 -0700

    Go Port Scanner

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Go-Port-Scanner
 A Go port scanner that simple tries to connect to the given address with a range of ports. 
+# Usage
+go build
+./go ipAddress startPort endPort timeoutMilliseconds 
---
+++ portScanner.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
+var maxPortNumber int = 65536
+
+func main() {
+	// Passed in arguments
+	ipAddress := os.Args[1]
+	portNumberStart, _ := strconv.Atoi(os.Args[2])
+	portNumberEnd, _ := strconv.Atoi(os.Args[3])
+	timeout, _ := strconv.Atoi(os.Args[4])
+
+	for i := portNumberStart; i <= portNumberEnd; i++ {
+		fullAddress := ipAddress + ":" + strconv.Itoa(i)
+		// Dial the network:port we want to examine
+		conn, err := net.DialTimeout("tcp", fullAddress, time.Duration(timeout)*time.Millisecond)
+		if err != nil { // If the port is not open or is filtered
+			fmt.Println("Dial error ", err.Error())
+		} else { // The port is open
+			fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n") // Make the request by writing to the conn
+			var buf bytes.Buffer
+			io.Copy(&buf, conn) // Copy the whole contents of the response
+			fmt.Println(fullAddress + " responded with " + strconv.Itoa(buf.Len()) + " bytes")
+			conn.Close() // Close the connection
+		}
+	}
+}
